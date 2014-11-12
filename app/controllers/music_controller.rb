e817f2048d1abebcafbb0e66207f6f1e393b0252
iliastsangaris <iliastsangaris@gmail.com>
2014-11-12 15:51:42 -0800

    updates

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -1,5 +1,5 @@
 class MusicController < ApplicationController
-  skip_before_filter :verify_authenticity_token, :only => [:curlMe]
+  skip_before_filter :verify_authenticity_token, :only => [:curlme]
 
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get '/search' => "music#search"
 
-  post '/curlme' => "music#curlMe"
+  post '/curlme' => "music#curlme"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
